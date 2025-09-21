d14e5849eee82039b032021ec051a83fbba6bfe6
ttddww <woldeteketel@gmail.com>
2025-09-21 16:35:12 -0400

    package update

--- Backend/db/dbConfig.js
+++ Backend/db/dbConfig.js
@@ -1,11 +1,19 @@
-const mysql2 = require("mysql2");
+const mysql = require("mysql2");
 
-const dbConnection = mysql2.createPool({
-  host: process.env.DB_HOST || "localhost",
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 10,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("DB connection failed:", err);
+    return; // prevent crash
+  }
+  console.log("DB connected!");
 });
 
 module.exports = dbConnection.promise();
