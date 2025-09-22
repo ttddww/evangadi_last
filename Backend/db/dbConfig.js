244fb68ac76ff815d14ad18bc9aee13b112cebae
ttddww <woldeteketel@gmail.com>
2025-09-21 22:48:42 -0400

    package update

--- Backend/db/dbConfig.js
+++ Backend/db/dbConfig.js
@@ -1,30 +1,16 @@
-const mysql = require("mysql2");
+const mysql2 = require("mysql2");
 
 // Create a connection pool
-const dbConnection = mysql.createPool({
-  host: process.env.DB_HOST,
+const dbConnection = mysql2.createPool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 3306,
   connectionLimit: 10,
-  queueLimit: 0,
 });
+
 
 // Export the connection with promises
 module.exports = dbConnection.promise();
 
-
-
-
-// const mysql2 = require("mysql2");
-
-// const dbConnection = mysql2.createPool({
-//   host: process.env.DB_HOST, // e.g. Render DB hostname
-//   user: process.env.DB_USER, // your DB username
-//   password: process.env.DB_PASS, // your DB password
-//   database: process.env.DB_NAME, // your DB name
-//   connectionLimit: 10,
-// });
-
-// module.exports = dbConnection.promise();
