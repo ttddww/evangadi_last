4674b0c2d2282a6eebbe9c6f6be5ec711195b8aa
ttddww <woldeteketel@gmail.com>
2025-09-23 01:00:20 -0400

    frontend updating api

--- frontend/src/assets/axiosConfig.js
+++ frontend/src/assets/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosBase = axios.create({
-  baseURL: "http://localhost:3000/api",
-  // baseURL: "https://evangadi04.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://evangadi04.onrender.com/api",
 });
 
 export default axiosBase;
